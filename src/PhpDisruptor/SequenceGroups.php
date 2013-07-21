2020ffeec276ec1accd9c7e5113e4319d253a837
prolic <saschaprolic@googlemail.com>
2013-07-21 16:54:12 +0200

    added dummy sequence groups class

---
+++ src/PhpDisruptor/SequenceGroups.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace PhpDisruptor;
+
+use Zend\Cache\Storage\StorageInterface;
+
+abstract class SequenceGroups
+{
+    /**
+     * @param SequencerInterface $sequencer
+     * @param StorageInterface $storage
+     * @param CursoredInterface $cursor
+     * @param Sequence[] $sequencesToAdd
+     */
+    public static function addSequences(
+        SequencerInterface $sequencer, // holder
+        StorageInterface $storage, // updater
+        CursoredInterface $cursor,
+        $sequencesToAdd
+    ) {
+
+        $updatedSequences = array();
+        $currentSequences = array();
+
+      /*
+    {
+        long cursorSequence;
+        Sequence[] updatedSequences;
+        Sequence[] currentSequences;
+
+        do
+        {
+            currentSequences = updater.get(holder);
+            updatedSequences = copyOf(currentSequences, currentSequences.length + sequencesToAdd.length);
+            cursorSequence = cursor.getCursor();
+
+            int index = currentSequences.length;
+            for (Sequence sequence : sequencesToAdd)
+            {
+                sequence.set(cursorSequence);
+                updatedSequences[index++] = sequence;
+            }
+        }
+        while (!updater.compareAndSet(holder, currentSequences, updatedSequences));
+
+        cursorSequence = cursor.getCursor();
+        for (Sequence sequence : sequencesToAdd)
+        {
+            sequence.set(cursorSequence);
+        }
+
+         */
+    }
+}
