b07f75590129507994f99be6883f4591a097b19e
prolic <saschaprolic@googlemail.com>
2013-07-25 16:23:23 +0200

    add lock interface

---
+++ src/PhpDisruptor/BlockingWaitStrategy.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: sasa
+ * Date: 25.07.13
+ * Time: 13:30
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ src/PhpDisruptor/ReentrantLock.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: sasa
+ * Date: 25.07.13
+ * Time: 15:25
+ * To change this template use File | Settings | File Templates.
+ */
