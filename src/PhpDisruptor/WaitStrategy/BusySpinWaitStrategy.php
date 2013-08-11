d1335501b44c957ae32cc76bb05c9186be62b2d6
prolic <saschaprolic@googlemail.com>
2013-08-11 18:18:57 +0200

    add phpdoc to busyspinwaitstrategy

--- src/PhpDisruptor/WaitStrategy/BusySpinWaitStrategy.php
+++ src/PhpDisruptor/WaitStrategy/BusySpinWaitStrategy.php
@@ -6,6 +6,12 @@ use PhpDisruptor\Exception;
 use PhpDisruptor\Sequence;
 use PhpDisruptor\SequenceBarrierInterface;
 
+/**
+ * Busy Spin strategy that uses a busy spin loop for EventProcessors waiting on a barrier.
+ *
+ * This strategy will use CPU resource to avoid syscalls which can introduce latency jitter.  It is best
+ * used when threads can be bound to specific CPU cores.
+ */
 class BusySpinWaitStrategy implements WaitStrategyInterface
 {
     /**
