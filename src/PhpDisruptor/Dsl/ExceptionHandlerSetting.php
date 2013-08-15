1065941929e2192125d9e7b54c121ada44c891ef
prolic <saschaprolic@googlemail.com>
2013-08-15 20:43:31 +0200

    implement exception handler setting

---
+++ src/PhpDisruptor/Dsl/ExceptionHandlerSetting.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace PhpDisruptor\Dsl;
+
+use PhpDisruptor\EventClassCapableInterface;
+use PhpDisruptor\EventHandlerInterface;
+use PhpDisruptor\Exception;
+use PhpDisruptor\ExceptionHandlerInterface;
+
+class ExceptionHandlerSetting implements EventClassCapableInterface
+{
+    /**
+     * @var string
+     */
+    protected $eventClass;
+
+    /**
+     * @var EventHandlerInterface
+     */
+    protected $eventHandler;
+
+    /**
+     * @var ConsumerRepository
+     */
+    protected $consumerRepository;
+
+    /**
+     * Constructor
+     *
+     * @param EventHandlerInterface $eventHandler
+     * @param ConsumerRepository $consumerRepository
+     * @throws Exception\InvalidArgumentException if event handler and consumer repository differ in event classes
+     */
+    public function __construct(EventHandlerInterface $eventHandler, ConsumerRepository $consumerRepository)
+    {
+        if ($eventHandler->getEventClass() != $consumerRepository->getEventClass()) {
+            throw new Exception\InvalidArgumentException(
+                '$consumerRepository uses event class ' . $consumerRepository->getEventClass()
+                . ' but $eventHandler uses event class ' . $eventHandler->getEventClass()
+            );
+        }
+        $this->eventClass = $eventHandler->getEventClass();
+        $this->eventHandler = $eventHandler;
+        $this->consumerRepository = $consumerRepository;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getEventClass()
+    {
+        return $this->eventClass;
+    }
+
+    /**
+     * Specify the ExceptionHandler to use with the event handler
+     *
+     * @param ExceptionHandlerInterface $exceptionHandler
+     * @return void
+     */
+    public function with(ExceptionHandlerInterface $exceptionHandler)
+    {
+        $this->consumerRepository->getEventProcessorFor($this->eventHandler)->setExceptionHandler($exceptionHandler);
+        $this->consumerRepository->getBarrierFor($this->eventHandler)->alert();
+    }
+}
