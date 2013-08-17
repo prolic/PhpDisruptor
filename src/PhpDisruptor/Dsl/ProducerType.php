35f7361363a9c9ba284e6117f3731e38d7a6bf74
prolic <saschaprolic@googlemail.com>
2013-08-17 20:31:16 +0200

    make producer type final

--- src/PhpDisruptor/Dsl/ProducerType.php
+++ src/PhpDisruptor/Dsl/ProducerType.php
@@ -5,7 +5,7 @@ namespace PhpDisruptor\Dsl;
 use Mabe_Enum as Enum;
 use PhpDisruptor\Exception;
 
-class ProducerType extends Enum
+final class ProducerType extends Enum
 {
     const SINGLE = 0;
     const MULTI  = 1;
