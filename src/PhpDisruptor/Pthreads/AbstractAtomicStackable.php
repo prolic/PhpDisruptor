5aaa7c2d8951c815dbfcefe396913a0491be2afe
prolic <saschaprolic@googlemail.com>
2013-09-14 15:37:56 +0200

    update atomic stackable

--- src/PhpDisruptor/Pthreads/AbstractAtomicStackable.php
+++ src/PhpDisruptor/Pthreads/AbstractAtomicStackable.php
@@ -2,4 +2,34 @@
 
 namespace PhpDisruptor\Pthreads;
 
-abstract class
+use Stackable;
+
+abstract class AbstractAtomicStackable extends Stackable
+{
+    public function run()
+    {
+    }
+
+    /**
+     * Performs a compare and swap operation on a class member
+     *
+     * @param $member
+     * @param $oldValue
+     * @param $newValue
+     * @return bool
+     */
+    public function casMember($member, $oldValue, $newValue)
+    {
+        $set = false;
+
+        $this->lock();
+        if ($this[$member] == $oldValue) {
+            $this[$member] = $newValue;
+
+            $set = true;
+        }
+        $this->unlock();
+
+        return $set;
+    }
+}
