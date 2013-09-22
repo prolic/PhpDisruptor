e154d329c5e26e7d9819145ac62a149d75b6f50a
prolic <saschaprolic@googlemail.com>
2013-09-22 21:49:44 +0200

    add multi producer sequencer test

---
+++ tests/PhpDisruptorTest/MultiProducerSequencerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PhpDisruptorTest;
+
+use PhpDisruptor\MultiProducerSequencer;
+use PhpDisruptor\WaitStrategy\BlockingWaitStrategy;
+
+class MultiProducerSequencerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testShouldOnlyAllowMessagesToBeAvailableIfSpecificallyPublished()
+    {
+        $publisher = new MultiProducerSequencer(1024, new BlockingWaitStrategy());
+        $publisher->publish(3);
+        $publisher->publish(5);
+        
+        $this->assertFalse($publisher->isAvailable(0));
+        $this->assertFalse($publisher->isAvailable(1));
+        $this->assertFalse($publisher->isAvailable(2));
+        $this->assertTrue($publisher->isAvailable(3));
+        $this->assertFalse($publisher->isAvailable(4));
+        $this->assertTrue($publisher->isAvailable(5));
+        $this->assertFalse($publisher->isAvailable(6));
+    }    
+}
