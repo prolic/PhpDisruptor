c7f572a9810b87b2c69beef44559f53e9f198898
prolic <saschaprolic@googlemail.com>
2013-09-23 19:45:28 +0200

    add aggregate event handler test

---
+++ tests/PhpDisruptorTest/AggregateEventHandlerTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace PhpDisruptorTest;
+
+use PhpDisruptor\AggregateEventHandler;
+use PHPUnit_Framework_MockObject_MockObject;
+
+class AggregateEventHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $eventHandlerOne;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $eventHandlerTwo;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $eventHandlerThree;
+
+    protected function setUp()
+    {
+        $this->eventHandlerOne   = $this->getMock('PhpDisruptor\LifecycleAwareEventHandler', array('onEvent'), array('stdClass'));
+        $this->eventHandlerTwo   = $this->getMock('PhpDisruptor\LifecycleAwareEventHandler', array('onEvent'), array('stdClass'));
+        $this->eventHandlerThree = $this->getMock('PhpDisruptor\LifecycleAwareEventHandler', array('onEvent'), array('stdClass'));
+    }
+
+    public function testShouldCallOnEventInSequence()
+    {
+        $event = new \stdClass();
+        $sequence = 3;
+        $endOfBatch = true;
+
+        $this->eventHandlerOne->expects($this->once())->method('onEvent')->will($this->returnCallback(function() { echo '1'; }));
+        $this->eventHandlerTwo->expects($this->once())->method('onEvent')->will($this->returnCallback(function() { echo '2'; }));
+        $this->eventHandlerThree->expects($this->once())->method('onEvent')->will($this->returnCallback(function() { echo '3'; }));
+
+        $aggregateEventHandler = new AggregateEventHandler(
+            'stdClass',
+            array(
+                $this->eventHandlerOne,
+                $this->eventHandlerTwo,
+                $this->eventHandlerThree
+            )
+        );
+
+        ob_start();
+        $aggregateEventHandler->onEvent($event, $sequence, $endOfBatch);
+        $result = ob_get_clean();
+        $this->assertEquals('123', $result);
+    }
+
+}
