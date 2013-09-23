6701e167ed3510310a2e2f4688702f952fc18ca9
prolic <saschaprolic@googlemail.com>
2013-09-23 19:24:39 +0200

    implement lifecycleawareeventhandler

---
+++ src/PhpDisruptor/LifecycleAwareEventHandler.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace PhpDisruptor;
+
+class LifecycleAwareEventHandler implements EventHandlerInterface, LifecycleAwareInterface
+{
+    /**
+     * @var string
+     */
+    protected $eventClass;
+
+    /**
+     * Constructor
+     *
+     * @param string $eventClass
+     * @throws Exception\InvalidArgumentException
+     */
+    public function __construct($eventClass)
+    {
+        if (!class_exists($eventClass)) {
+            throw new Exception\InvalidArgumentException(
+                'Event class ' . $eventClass . ' not found'
+            );
+        }
+        $this->eventClass = $eventClass;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getEventClass()
+    {
+        return $this->eventClass;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function onEvent($event, $sequence, $endOfBatch)
+    {
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function onStart()
+    {
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function onShutdown()
+    {
+    }
+}
