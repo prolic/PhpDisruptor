2deb537604d9ff0fcf7a7085791729fd5dd08f8b
prolic <saschaprolic@googlemail.com>
2013-09-25 20:25:56 +0200

    add more test assets

--- tests/PhpDisruptorTest/TestAsset/EventHandler.php
+++ tests/PhpDisruptorTest/TestAsset/EventHandler.php
@@ -1,8 +1,45 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: sasa
- * Date: 25.09.13
- * Time: 19:50
- * To change this template use File | Settings | File Templates.
- */
+
+namespace PhpDisruptorTest\TestAsset;
+
+use PhpDisruptor\EventHandlerInterface;
+use PhpDisruptor\Exception;
+use Stackable;
+
+class EventHandler extends Stackable implements EventHandlerInterface
+{
+    protected $eventClass;
+
+    public function __construct($eventClass)
+    {
+        $this->eventClass = $eventClass;
+    }
+
+    public function run()
+    {
+    }
+
+    /**
+     * Return the used event class name
+     *
+     * @return string
+     */
+    public function getEventClass()
+    {
+        return $this->eventClass;
+    }
+
+    /**
+     * Called when a publisher has published an event to the RingBuffer
+     *
+     * @param object $event published to the RingBuffer
+     * @param int $sequence of the event being processed
+     * @param bool $endOfBatch flag to indicate if this is the last event in a batch from the RingBuffer
+     * @return void
+     * @throws Exception\ExceptionInterface if the EventHandler would like the exception handled further up the chain.
+     */
+    public function onEvent($event, $sequence, $endOfBatch)
+    {
+        echo get_class($event) . '-' . $sequence . '-' . (string) (int) $endOfBatch;
+    }
+}
--- tests/PhpDisruptorTest/TestAsset/EventProcessor.php
+++ tests/PhpDisruptorTest/TestAsset/EventProcessor.php
@@ -1,8 +1,37 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: sasa
- * Date: 25.09.13
- * Time: 20:20
- * To change this template use File | Settings | File Templates.
- */
+
+namespace PhpDisruptorTest\TestAsset;
+
+use PhpDisruptor\EventProcessor\AbstractEventProcessor;
+use PhpDisruptor\Sequence;
+
+class EventProcessor extends AbstractEventProcessor
+{
+    protected $sequence;
+
+    public function setSequence(Sequence $sequence)
+    {
+        $this->sequence = $sequence;
+    }
+
+    /**
+     * Get a reference to the Sequence being used by this EventProcessor.
+     *
+     * @return Sequence reference to the Sequence for this EventProcessor
+     */
+    public function getSequence()
+    {
+        return $this->sequence;
+    }
+
+    /**
+     * @return void
+     */
+    public function halt()
+    {
+    }
+
+    public function run()
+    {
+    }
+}
