ae06ddb83d39866f9bd9b3f74d6479cbbcbf9668
prolic <saschaprolic@googlemail.com>
2013-10-06 22:30:50 +0200

    add ring buffer test

---
+++ tests/PhpDisruptorTest/RingBufferTest.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: sasa
+ * Date: 06.10.13
+ * Time: 22:16
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ tests/PhpDisruptorTest/TestAsset/StubEventTranslator.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace PhpDisruptorTest\TestAsset;
+
+use PhpDisruptor\EventProcessor\AbstractEventProcessor;
+use PhpDisruptor\Sequence;
+
+class StubEventProcessor extends AbstractEventProcessor
+{
+    /**
+     * @var Sequence
+     */
+    protected $sequence;
+
+    public function __construct()
+    {
+        $this->sequence = new Sequence();
+    }
+
+    public function setSequence($sequence)
+    {
+        $this->sequence->set($sequence);
+    }
+
+    /**
+     * Get a reference to the Sequence being used by this EventProcessor.
+     *
+     * @return Sequence reference to the Sequence for this EventProcessor
+     */
+    public function getSequence()
+    {
+        return $this->sequence;
+    }
+
+    /**
+     * @return void
+     */
+    public function halt()
+    {
+    }
+
+    public function run()
+    {
+    }
+}
