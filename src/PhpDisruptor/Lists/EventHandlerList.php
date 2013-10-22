b8463f7001204e44a0a29d772182e93a8ca40e99
prolic <saschaprolic@googlemail.com>
2013-10-22 23:46:39 +0200

    add eventhandler list

---
+++ src/PhpDisruptor/Lists/EventHandlerList.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PhpDisruptor\Lists;
+
+use PhpDisruptor\EventHandlerInterface;
+use PhpDisruptor\Pthreads\StackableArray;
+
+class EventHandlerList extends StackableArray
+{
+    public $list;
+
+    /**
+     * @param EventHandlerInterface|array $entities
+     */
+    public function __construct($entities)
+    {
+        if ($entities instanceof Seq) {
+            $this->add($entities);
+        } else if (is_array($entities)) {
+            foreach ($entities as $entity) {
+                $this->add($entity);
+            }
+        }
+    }
+
+    public function add(EventHandlerInterface $entity)
+    {
+        $this->list[] = $entity;
+    }
+}
