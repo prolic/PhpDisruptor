7ed834a6757dac28a15851cb34e1b0afc180b701
prolic <saschaprolic@googlemail.com>
2014-02-08 21:29:01 +0100

    add sequenceupdater testasset

---
+++ tests/PhpDisruptorTest/TestAsset/SequenceUpdater.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PhpDisruptorTest\TestAsset;
+
+use PhpDisruptor\Pthreads\CyclicBarrier;
+use PhpDisruptor\Sequence;
+use PhpDisruptor\WaitStrategy\WaitStrategyInterface;
+
+class SequenceUpdater extends \Thread
+{
+    public $sequence;
+
+    public $barrier;
+
+    public $sleepTimeMicros;
+
+    public $waitStrategy;
+
+    public function __construct($sleepTimeMicros, WaitStrategyInterface $waitStrategy)
+    {
+        $this->sleepTimeMicros = $sleepTimeMicros;
+        $this->waitStrategy = $waitStrategy;
+        $this->barrier = new CyclicBarrier(2);
+        $this->sequence = new Sequence();
+    }
+
+    public function run()
+    {
+        $this->barrier->await();
+        if (0 != $this->sleepTimeMicros) {
+            $this->wait($this->sleepTimeMicros);
+        }
+        $this->sequence->incrementAndGet();
+        $this->waitStrategy->signalAllWhenBlocking();
+    }
+
+    public function waitForStartup()
+    {
+        $this->barrier->await();
+    }
+}
