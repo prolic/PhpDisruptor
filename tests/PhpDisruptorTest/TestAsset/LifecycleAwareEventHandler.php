b799cd839e68ac41c887005c00fcd92ce7038e67
prolic <saschaprolic@googlemail.com>
2014-02-10 16:15:02 +0100

    add lifecycleaware testasset, improve exceptionhandler tests

--- tests/PhpDisruptorTest/TestAsset/LifecycleAwareEventHandler.php
+++ tests/PhpDisruptorTest/TestAsset/LifecycleAwareEventHandler.php
@@ -5,36 +5,47 @@ namespace PhpDisruptorTest\TestAsset;
 use PhpDisruptor\EventHandlerInterface;
 use PhpDisruptor\Exception;
 use PhpDisruptor\LifecycleAwareInterface;
+use PhpDisruptor\Pthreads\CountDownLatch;
+use PhpDisruptor\Pthreads\StackableArray;
 use PhpDisruptor\Pthreads\UuidStackable;
 
-class LifecycleAwareEventHandler extends UuidStackable implements EventHandlerInterface, LifecycleAwareInterface
+class LifecycleAwareEventHandler extends StackableArray implements EventHandlerInterface, LifecycleAwareInterface
 {
-    /**
-     * @inheritdoc
-     */
-    public function getEventClass()
+    public $startCounter;
+
+    public $shutdownCounter;
+
+    public $startLatch;
+
+    public $shutdownLatch;
+
+    public function __construct(CountDownLatch $startLatch, CountDownLatch $shutdownLatch)
     {
-        return 'stdClass';
+        $this->startCounter = 0;
+        $this->shutdownCounter = 0;
+
+        $this->startLatch = $startLatch;
+        $this->shutdownLatch = $shutdownLatch;
     }
 
-    /**
-     * @inheritdoc
-     */
+    public function getEventClass()
+    {
+        return __NAMESPACE__ . '\StubEvent';
+    }
+
     public function onEvent($event, $sequence, $endOfBatch)
     {
     }
 
-    /**
-     * @inheritdoc
-     */
     public function onStart()
     {
+        ++$this->startCounter;
+        $this->startLatch->countDown();
     }
 
-    /**
-     * @inheritdoc
-     */
     public function onShutdown()
     {
+        ++$this->shutdownCounter;
+        $this->shutdownLatch->countDown();
     }
 }
