68d2d0171526285aae684fdf01adcc92a287d4c6
prolic <saschaprolic@googlemail.com>
2014-02-10 16:37:46 +0100

    add sequencereportinghandler test

---
+++ tests/PhpDisruptorTest/SequenceReportingCallbackTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace PhpDisruptorTest;
+
+use PhpDisruptor\EventProcessor\BatchEventProcessor;
+use PhpDisruptor\Lists\SequenceList;
+use PhpDisruptor\Pthreads\CountDownLatch;
+use PhpDisruptor\RingBuffer;
+use PhpDisruptorTest\TestAsset\StubEventFactory;
+use PhpDisruptorTest\TestAsset\TestSequenceReportingEventHandler;
+
+class SequenceReportingCallbackTest extends \PHPUnit_Framework_TestCase
+{
+    private $callbackLatch;
+
+    /**
+     * @var CountDownLatch
+     */
+    private $onEndOfBatchLatch;
+
+    public function testShouldReportProgressByUpdatingSequenceViaCallback()
+    {
+        $callbackLatch = new CountDownLatch(1);
+        $onEndOfBatchLatch = new CountDownLatch(1);
+
+        $eventFactory = new StubEventFactory();
+        $ringBuffer = RingBuffer::createMultiProducer($eventFactory, 16);
+        $sequenceBarrier = $ringBuffer->newBarrier();
+        $handler = new TestSequenceReportingEventHandler($callbackLatch);
+        $batchEventProcessor = new BatchEventProcessor($eventFactory->getEventClass(), $ringBuffer, $sequenceBarrier, $handler);
+
+        $sequenceList = new SequenceList($batchEventProcessor->getSequence());
+        $ringBuffer->addGatingSequences($sequenceList);
+
+        $batchEventProcessor->start();
+
+        $this->assertSame(-1, $batchEventProcessor->getSequence()->get());
+        $ringBuffer->publish($ringBuffer->next());
+
+        $callbackLatch->await();
+        $this->assertSame(0, $batchEventProcessor->getSequence()->get());
+
+        $onEndOfBatchLatch->countDown();
+        $this->assertSame(0, $batchEventProcessor->getSequence()->get());
+
+        $batchEventProcessor->halt();
+        $batchEventProcessor->join();
+    }
+}
---
+++ tests/PhpDisruptorTest/TestAsset/TestSequenceReportingEventHandler.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PhpDisruptorTest\TestAsset;
+
+use PhpDisruptor\Pthreads\CountDownLatch;
+use PhpDisruptor\Sequence;
+use PhpDisruptor\SequenceReportingEventHandlerInterface;
+
+class TestSequenceReportingEventHandler implements SequenceReportingEventHandlerInterface
+{
+    /**
+     * @var CountDownLatch
+     */
+    public $latch;
+
+    /**
+     * @var Sequence
+     */
+    public $sequenceCallback;
+
+    public function __construct(CountDownLatch $latch)
+    {
+        $this->latch = $latch;
+    }
+
+    public function getEventClass()
+    {
+        return __NAMESPACE__ . '\StubEvent';
+    }
+
+    public function setSequenceCallback(Sequence $sequenceTrackerCallback)
+    {
+        $this->sequenceCallback = $sequenceTrackerCallback;
+    }
+
+    public function onEvent($event, $sequence, $endOfBatch)
+    {
+        $this->sequenceCallback->set($sequence);
+        $this->latch->countDown();
+    }
+}
