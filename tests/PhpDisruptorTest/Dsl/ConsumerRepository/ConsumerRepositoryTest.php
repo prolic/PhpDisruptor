abae80c1a343494bfe58cfbbaab5d11cc6ad5970
prolic <saschaprolic@googlemail.com>
2014-07-01 21:11:15 +0200

    ConsumerRepositoryTest

--- tests/PhpDisruptorTest/Dsl/ConsumerRepository/ConsumerRepositoryTest.php
+++ tests/PhpDisruptorTest/Dsl/ConsumerRepository/ConsumerRepositoryTest.php
@@ -5,11 +5,11 @@ namespace PhpDisruptorTest\Dsl\ConsumerRepository;
 use PhpDisruptor\Dsl\ConsumerRepository;
 use PhpDisruptor\EventFactoryInterface;
 use PhpDisruptor\EventProcessor\AbstractEventProcessor;
-use ConcurrentPhpUtils\NoOpStackable;
+use PhpDisruptor\Lists\SequenceList;
 use PhpDisruptor\RingBuffer;
 use PhpDisruptor\Sequence;
 use PhpDisruptor\SequenceBarrierInterface;
-use PhpDisruptorTest\Dsl\ConsumerRepository\TestAsset\SleepingEventHandler;
+use PhpDisruptorTest\TestAsset\SleepingEventHandler;
 use PhpDisruptorTest\TestAsset\TestEventFactory;
 use PhpDisruptorTest\TestAsset\TestEventProcessor;
 
@@ -77,7 +77,7 @@ class ConsumerRepositoryTest extends \PHPUnit_Framework_TestCase
     public function testShouldGetBarrierByHandler()
     {
         $this->consumerRepository->addEventProcessor($this->eventProcessor1, $this->handler1, $this->barrier1);
-        $this->assertTrue($this->barrier1->equals($this->consumerRepository->getBarrierFor($this->handler1)));
+        $this->assertSame($this->barrier1, $this->consumerRepository->getBarrierFor($this->handler1));
     }
 
     public function testShouldReturnNullForBarrierWhenHandlerIsNotRegistered()
@@ -90,19 +90,19 @@ class ConsumerRepositoryTest extends \PHPUnit_Framework_TestCase
         $this->consumerRepository->addEventProcessor($this->eventProcessor1, $this->handler1, $this->barrier1);
         $this->consumerRepository->addEventProcessor($this->eventProcessor2, $this->handler2, $this->barrier2);
 
-        $data = new NoOpStackable();
-        $data[] = $this->eventProcessor2->getSequence();
-        $this->consumerRepository->unMarkEventProcessorsAsEndOfChain($data);
+        $sequences = new SequenceList();
+        $sequences[] = $this->eventProcessor2->getSequence();
+        $this->consumerRepository->unMarkEventProcessorsAsEndOfChain($sequences);
 
         $lastEventProcessorsInChain = $this->consumerRepository->getLastSequenceInChain(true);
         $this->assertEquals(1, count($lastEventProcessorsInChain));
-        $this->assertTrue($this->eventProcessor1->getSequence()->equals($lastEventProcessorsInChain[0]));
+        $this->assertTrue($this->eventProcessor1->getSequence() === $lastEventProcessorsInChain[0]);
     }
 
     public function testShouldRetrieveEventProcessorForHandler()
     {
         $this->consumerRepository->addEventProcessor($this->eventProcessor1, $this->handler1, $this->barrier1);
-        $this->assertTrue($this->consumerRepository->getEventProcessorFor($this->handler1)->equals($this->eventProcessor1));
+        $this->assertTrue($this->consumerRepository->getEventProcessorFor($this->handler1) === $this->eventProcessor1);
     }
 
     /**
@@ -126,13 +126,13 @@ class ConsumerRepositoryTest extends \PHPUnit_Framework_TestCase
 
         foreach($this->consumerRepository->getConsumerInfos() as $info) {
             if (!$seen1
-                && $info->getEventProcessor()->equals($this->eventProcessor1)
-                && $info->getHandler()->equals($this->handler1)
+                && $info->getEventProcessor() === $this->eventProcessor1
+                && $info->getHandler() === $this->handler1
             ) {
                 $seen1 = true;
             } else if (!$seen2
-                && $info->getEventProcessor()->equals($this->eventProcessor2)
-                && $info->getHandler()->equals($this->handler2)
+                && $info->getEventProcessor() === $this->eventProcessor2
+                && $info->getHandler() === $this->handler2
             ) {
                 $seen2  = true;
             } else {
